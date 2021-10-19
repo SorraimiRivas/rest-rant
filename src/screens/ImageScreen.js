05ee7db05d1caa1f7f66e92f9c5c5d7a55414f1c
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2021-10-19 16:29:14 -0400

    Added state for text changes on search bar

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <View></View>
+    </View>
+  );
+};
+
+export default ImageScreen;
